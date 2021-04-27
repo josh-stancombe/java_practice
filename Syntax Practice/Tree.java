f6b934b98c7a9b3de3caad4a02909f3aadab3bc4
Josh Stancombe <61159870+josh-stancombe@users.noreply.github.com>
2021-04-27 08:25:10 +0100

    Add files via upload

---
+++ Syntax Practice/Tree.java
@@ -0,0 +1,15 @@
+// Syntax practice - draw a tree...
+
+public class Tree {
+  public static void main(String[] args){
+    // This will plant a tree
+    System.out.println("Hello I am Java");
+    System.out.println("I am going to plant a tree...");
+    System.out.println("    *  ");
+    System.out.println("   *** ");
+    System.out.println("  ***** ");
+    System.out.println(" ******* ");
+    System.out.println("    *  ");
+    System.out.println("    *  ");   
+  }
+}
